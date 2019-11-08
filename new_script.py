4daa8e2118eb7f507b1e337d250983987fb726a4
theo123490 <theo123490@gmail.com>
2019-11-08 19:10:01 +0700

    change branch onl

--- new_script.py
+++ new_script.py
@@ -1,10 +1,8 @@
 import numpy as np
 
 a = np.array([1,2,3,4])
-that_list = ['a','b','hehehehehe','d']
+that_list = ['ini','adalah','new','fing','branch']
 
 #ini commit setelah pull 
 for i in that_list:
     print(i)
-
-print('is this in a new branch')
